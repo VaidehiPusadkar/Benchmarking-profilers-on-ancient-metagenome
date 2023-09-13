0a1ae2c0106efda2ae3a43fb27fedc4603002640
Vaidehi Pusadkar <98483312+VaidehiPusadkar@users.noreply.github.com>
2023-09-12 19:22:38 -0500

    Update simulation_aDNA_metagenome.sh

--- simulation_aDNA_metagenome.sh
+++ simulation_aDNA_metagenome.sh
@@ -1,7 +1,7 @@
 #
-##Simulation-1 - Deamination
 # input folder contains three folders  called bact (containing the microbial species), cont (containing the contaminant modern human / microbial sequences), endo (containing the endogenous ancient human sequences)
 # Five replicates of each sample were generated
+##Simulation-1 - Deamination
 
 ./gargammel.pl  --comp 0.99,0,0.01 -n 5000000 --minsize 35 -l 125 -rl 125 -damage 0.036,0.36,0.009,0.8 -se -ss HS25 -o /simulation1/simulation_damage_high_data/ input_folder/
 
