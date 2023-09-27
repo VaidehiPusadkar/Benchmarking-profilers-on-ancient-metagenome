6680a57cae650dc41c95f7b1d656bd0ecdc4940e
Vaidehi Pusadkar <98483312+VaidehiPusadkar@users.noreply.github.com>
2023-09-27 01:09:12 -0500

    Update Simulation_2.sh

--- simulation/Simulation_2.sh
+++ simulation/Simulation_2.sh
@@ -1,6 +1,7 @@
 #
 # input folder contains three folders  called bact (containing the microbial species), cont (containing the contaminant modern human / microbial sequences), endo (containing the endogenous ancient human sequences)
 # Five replicates of each sample were generated
+# The following commands generate the 5M (-n) single-stranded (-ss) metagenome reads on a HiSeq 2500 Illumina run. Differing rates of Deamination, fragment length, and contamination are donated by -damage, -l, and -comp, respectively. 
 ##Simulation-2a - Deamination + Fragment length
 
 ./gargammel.pl  --comp 0.99,0,0.01 -n 5000000 -l 40 -rl 40 -damage 0.036,0.36,0.009,0.8 -se -ss HS25 -o /simulation2/simulation_damage_high_data/ input_folder/
